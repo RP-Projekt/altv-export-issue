ca4ef14431df6a040b158e2c36ae6cda81ceaa76
Tertosie <origin62807@googlemail.com>
2023-09-12 11:37:58 +0200

    Initial Bug commit

---
+++ ExportBug/Main.cs
@@ -0,0 +1,22 @@
+﻿using AltV.Net;
+using AltV.Net.Async;
+
+namespace Main
+{
+    public class Main : AsyncResource
+    {
+        public Main() : base(false)
+        {
+            
+        }
+
+        public override void OnStart()
+        {
+            Alt.Export("broadcast", (string message) => { Alt.EmitAllClients("chatmessage", null, message); });
+        }
+
+        public override void OnStop()
+        {
+        }
+    }
+}
